59e5ff53ff046bd806e50e472dbd3f34027fb61c
Gergely Bekesi <gergely.bekesi@bitrise.io>
2018-11-30 09:53:34 +0100

    Implement step

--- main.go
+++ main.go
@@ -2,19 +2,52 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 )
 
 func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+	addonURL := "https://bitrise-sample-addon.herokuapp.com/"
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ascii-art/%s", addonURL, os.Getenv("BITRISE_APP_SLUG")), nil)
+	if err != nil {
+		fmt.Printf("Failed to create HTTP request: %s", err)
+		os.Exit(1)
+	}
+	accessKey, ok := os.LookupEnv("BITRISE_SAMPLE_ADDON_ACCESS_KEY")
+	if !ok {
+		fmt.Println("Failed to look up environment variable")
+		os.Exit(1)
+	}
+	req.Header.Add("Authentication", accessKey)
+
+	var client http.Client
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Failed to send HTTP request: %s", err)
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Failed to fetch ASCII art from add-on: %s", err)
+		os.Exit(1)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Failed to read response body: %s", err)
+		os.Exit(1)
+	}
+	asciiArt := string(bodyBytes)
 
 	//
 	// --- Step Outputs: Export Environment Variables for other Steps:
 	// You can export Environment Variables for other Steps with
 	//  envman, which is automatically installed by `bitrise setup`.
 	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "BITRISE_ASCII_ART", "--value", asciiArt, "-n").CombinedOutput()
 	if err != nil {
 		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
 		os.Exit(1)
