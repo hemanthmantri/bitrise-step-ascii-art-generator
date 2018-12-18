168728886ec7e93eca08c72e924f4e4266adfa73
Gergely Bekesi <gergely.bekesi@bitrise.io>
2018-12-18 14:08:27 +0100

    Refactor step to be more conventional

--- main.go
+++ main.go
@@ -5,52 +5,65 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"os/exec"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-tools/go-steputils/stepconf"
 )
 
+type config struct {
+	AddonURL       string `env:"addon_url"`
+	AccessToken    string `env:"access_token"`
+	BitriseAppSlug string `env:"BITRISE_APP_SLUG"`
+}
+
+func failf(msg string, args ...interface{}) {
+	log.Errorf(msg, args...)
+	os.Exit(1)
+}
+
 func main() {
-	addonURL, ok := os.LookupEnv("addon_url")
-	if !ok {
-		fmt.Println("No addon URL is specified")
-		os.Exit(1)
+	var cfg config
+	if err := stepconf.Parse(&cfg); err != nil {
+		failf("Issue with input: %s", err)
 	}
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ascii-art/%s", addonURL, os.Getenv("BITRISE_APP_SLUG")), nil)
+	stepconf.Print(cfg)
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ascii-art/%s", cfg.AddonURL, cfg.BitriseAppSlug), nil)
 	if err != nil {
-		fmt.Printf("Failed to create HTTP request: %s", err)
-		os.Exit(1)
+		failf("Failed to create HTTP request: %s", err)
 	}
-	accessKey, ok := os.LookupEnv("access_token")
-	if !ok {
-		fmt.Println("Failed to look up environment variable")
-		os.Exit(1)
-	}
-	req.Header.Add("Authentication", accessKey)
+
+	req.Header.Add("Authentication", cfg.AccessToken)
 
 	var client http.Client
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Printf("Failed to send HTTP request: %s", err)
-		os.Exit(1)
+		failf("Failed to send HTTP request: %s", err)
 	}
 
-	defer resp.Body.Close()
+	defer func() {
+		err := resp.Body.Close()
+		failf("Failed to close response body: %s", err)
+	}()
+
 	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Failed to fetch ASCII art from add-on: %s", err)
-		os.Exit(1)
+		failf("Failed to fetch ASCII art from add-on: %s", err)
 	}
 
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Printf("Failed to read response body: %s", err)
-		os.Exit(1)
+		failf("Failed to read response body: %s", err)
 	}
 	asciiArt := string(bodyBytes)
 
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "BITRISE_ASCII_ART", "--value", asciiArt, "-n").CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
-	}
+	cmdLog := command.New("bitrise", "envman", "add", "--key", "BITRISE_ASCII_ART", "--value", asciiArt, "-n")
 
-	os.Exit(0)
+	fmt.Println()
+	log.Donef("$ %s", cmdLog.PrintableCommandArgs())
+	fmt.Println()
+
+	if err := cmdLog.Run(); err != nil {
+		failf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+	}
 }
